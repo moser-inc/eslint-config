c9c4102654e52285f7565ffe1ccea26546614aa2
Alex Howard <alex.howard@moserit.com>
2022-06-02 14:29:14 -0400

    revert: "feat: Add airbnb and airbnb-base"
    This reverts commit 993b41eddbe64ff6f1486b35d9fb27b95ad0eaf0.

--- packages/core/index.js
+++ packages/core/index.js
@@ -7,7 +7,6 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'airbnb-base',
   ],
   ignorePatterns: ['node_modules', 'dist'],
   overrides: [
