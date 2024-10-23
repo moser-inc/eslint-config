9bfab2b4350079e14458745802c515878eb85e3c
Daniel Waltz <daniel.waltz@moserit.com>
2024-10-23 10:57:33 -0400

    feat(core): enable no import type side effects rule

--- packages/core/index.js
+++ packages/core/index.js
@@ -44,6 +44,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-import-type-side-effects': 'error',
   },
   overrides: [
     {
