4464acc8b4b49bb7cf8fd6b1acf969689fe4a8c1
Daniel Waltz <daniel.waltz@moserit.com>
2024-12-04 13:49:54 -0500

    chore: smoke test legacy configs

---
+++ packages/react/.eslintrc.cjs
@@ -0,0 +1,6 @@
+/** @type {import('eslint').Linter.LegacyConfig} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: ['@moser-inc/eslint-config-react'],
+  ignorePatterns: ['dist', 'build.config.ts', 'src/flat.ts'],
+};
