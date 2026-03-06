9836225ec14ac7a2c113f7472e1c6611c9741a34
Daniel Waltz <daniel.waltz@moserit.com>
2026-03-06 16:24:16 -0500

    chore: fix build

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -1,8 +1,4 @@
-import { appendFile } from 'node:fs/promises';
-import { builtinRules } from 'eslint/use-at-your-own-risk';
-import { flatConfigsToRulesDTS } from 'eslint-typegen/core';
 import { defineConfig } from 'tsdown';
-import moser from '@moser-inc/eslint-config';
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -11,48 +7,5 @@ export default defineConfig({
   deps: {
     neverBundle: ['eslint-flat-config-utils'],
   },
-  hooks: {
-    'build:done': async (ctx) => {
-      const config = await moser().append({
-        plugins: {
-          '': {
-            rules: Object.fromEntries(builtinRules),
-          },
-        },
-      });
-
-      let dts = await flatConfigsToRulesDTS(config, {
-        includeIgnoreComments: false,
-        includeTypeImports: false,
-      });
-
-      const configNames = [
-        ...new Set(
-          config
-            .map((item) => item.name)
-            .filter((name) => name !== undefined && !!name),
-        ),
-      ];
-
-      dts += `
-        declare module 'eslint-flat-config-utils' {
-          interface DefaultConfigNamesMap {
-            ${configNames.map((name) => `'${name}': true;`).join('\n')}
-          }
-        }
-      `;
-
-      for (const declarationFileName of ['index.d.mts']) {
-        const declarationFilePath = `${ctx.options.outDir}/${declarationFileName}`;
-
-        try {
-          await appendFile(declarationFilePath, dts);
-        } catch {
-          continue;
-        }
-      }
-
-      console.log('Core config type definitions generated');
-    },
-  },
+  onSuccess: 'node ./scripts/typegen.ts',
 });
---
+++ packages/react/scripts/typegen.ts
@@ -0,0 +1,45 @@
+import { appendFile } from 'node:fs/promises';
+import { builtinRules } from 'eslint/use-at-your-own-risk';
+import { flatConfigsToRulesDTS } from 'eslint-typegen/core';
+import moser from '../src/index.ts';
+
+const config = await moser().append({
+  plugins: {
+    '': {
+      rules: Object.fromEntries(builtinRules),
+    },
+  },
+});
+
+let dts = await flatConfigsToRulesDTS(config, {
+  includeIgnoreComments: false,
+  includeTypeImports: false,
+});
+
+const configNames = [
+  ...new Set(
+    config
+      .map((item) => item.name)
+      .filter((name) => name !== undefined && !!name),
+  ),
+];
+
+dts += `
+        declare module 'eslint-flat-config-utils' {
+          interface DefaultConfigNamesMap {
+            ${configNames.map((name) => `'${name}': true;`).join('\n')}
+          }
+        }
+      `;
+
+for (const declarationFileName of ['index.d.mts']) {
+  const declarationFilePath = `dist/${declarationFileName}`;
+
+  try {
+    await appendFile(declarationFilePath, dts);
+  } catch {
+    continue;
+  }
+}
+
+console.log('React config type definitions generated');
